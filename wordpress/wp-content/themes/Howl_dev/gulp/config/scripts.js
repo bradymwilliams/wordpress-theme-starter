763639ccbb78eadbc7a8f57ade6de40852945210
Brady Wlliams <bradymwilliams@gmail.com>
2016-11-01 16:52:50 -0600

    add jQuery as external dep-- temp!

--- wordpress/wp-content/themes/Howl_dev/gulp/config/scripts.js
+++ wordpress/wp-content/themes/Howl_dev/gulp/config/scripts.js
@@ -1,7 +1,24 @@
+var webpack = require('webpack');
+
 /**
  * Override the Default
  * Core Scripts
  * Config
  *
  */
-module.exports = {};
+module.exports = {
+    options: {
+        webpack: {
+            defaults: {
+                plugins: [
+                    new webpack.ProvidePlugin({
+                        '$': 'jquery'
+                    })
+                ],
+                externals: {
+                    jquery: 'window.jQuery'
+                }
+            }
+        }
+    }
+};
--- wordpress/wp-content/themes/Howl_dev/theme/functions.php
+++ wordpress/wp-content/themes/Howl_dev/theme/functions.php
@@ -7,7 +7,7 @@
 
 
 /* =========================================
-		ACTION HOOKS & FILTERS
+        ACTION HOOKS & FILTERS
    ========================================= */
 
 /**--- Actions ---**/
@@ -27,7 +27,7 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 
 /* =========================================
-		HOOKED Functions
+        HOOKED Functions
    ========================================= */
 
 /**--- Actions ---**/
@@ -39,91 +39,91 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
  * @since 1.0
  */
 if ( ! function_exists( 'theme_setup' ) ) {
-	function theme_setup() {
+    function theme_setup() {
 
-		// Let wp know we want to use html5 for content
-		add_theme_support( 'html5', array(
-			'comment-list',
-			'comment-form',
-			'search-form',
-			'gallery',
-			'caption'
-		) );
-
-
-		// Let wp know we want to use post thumbnails
-		/*
-		add_theme_support( 'post-thumbnails' );
-		*/
-		
-		// Add Custom Logo Support.
-		/*
-		add_theme_support( 'custom-logo', array(
-			'width'       => 181, // Example Width Size
-			'height'      => 42,  // Example Height Size
-			'flex-width'  => true,
-		) );
-		*/
-
-		// Register navigation menus for theme
-		/*
-		register_nav_menus( array(
-			'primary' => 'Main Menu',
-			'footer'  => 'Footer Menu'
-		) );
-		*/
+        // Let wp know we want to use html5 for content
+        add_theme_support( 'html5', array(
+            'comment-list',
+            'comment-form',
+            'search-form',
+            'gallery',
+            'caption'
+        ) );
 
 
-		// Let wp know we are going to handle styling galleries
-		/*
-		add_filter( 'use_default_gallery_style', '__return_false' );
-		*/
+        // Let wp know we want to use post thumbnails
+        /*
+        add_theme_support( 'post-thumbnails' );
+        */
+        
+        // Add Custom Logo Support.
+        /*
+        add_theme_support( 'custom-logo', array(
+            'width'       => 181, // Example Width Size
+            'height'      => 42,  // Example Height Size
+            'flex-width'  => true,
+        ) );
+        */
+
+        // Register navigation menus for theme
+        
+        register_nav_menus( array(
+            'primary' => 'Main Menu',
+            'footer'  => 'Footer Menu'
+        ) );
+        
 
 
-		// Stop WP from printing emoji service on the front
-		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        // Let wp know we are going to handle styling galleries
+        /*
+        add_filter( 'use_default_gallery_style', '__return_false' );
+        */
 
 
-		// Remove toolbar for all users in front end
-		show_admin_bar( false );
+        // Stop WP from printing emoji service on the front
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 
-		// Add Custom Image Sizes
-		/*
-		add_image_size( 'ExampleImageSize', 1200, 450, true ); // Example Image Size
-		...
-		*/
+        // Remove toolbar for all users in front end
+        show_admin_bar( false );
 
 
-		// WPML configuration
-		// disable plugin from printing styles and js
-		// we are going to handle all that ourselves.
-		if ( ! is_admin() ) {
-			define( 'ICL_DONT_LOAD_NAVIGATION_CSS', true );
-			define( 'ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true );
-			define( 'ICL_DONT_LOAD_LANGUAGES_JS', true );
-		}
+        // Add Custom Image Sizes
+        /*
+        add_image_size( 'ExampleImageSize', 1200, 450, true ); // Example Image Size
+        ...
+        */
 
 
-		// Contact Form 7 Configuration needs to be done
-		// in wp-config.php. add the following snippet
-		// under the line:
-		// define( 'WP_DEBUG', false );
-		/*
-		//Contact Form 7 Plugin Configuration
-		define ( 'WPCF7_LOAD_JS',  false ); // Added to disable JS loading
-		define ( 'WPCF7_LOAD_CSS', false ); // Added to disable CSS loading
-		define ( 'WPCF7_AUTOP',    false ); // Added to disable adding <p> & <br> in form output
-		*/
+        // WPML configuration
+        // disable plugin from printing styles and js
+        // we are going to handle all that ourselves.
+        if ( ! is_admin() ) {
+            define( 'ICL_DONT_LOAD_NAVIGATION_CSS', true );
+            define( 'ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true );
+            define( 'ICL_DONT_LOAD_LANGUAGES_JS', true );
+        }
 
 
-		// Register Autoloaders Loader
-		$theme_dir = get_template_directory();
-		include "$theme_dir/library/library-loader.php";
-		include "$theme_dir/includes/includes-loader.php";
-		include "$theme_dir/components/components-loader.php";
-	}
+        // Contact Form 7 Configuration needs to be done
+        // in wp-config.php. add the following snippet
+        // under the line:
+        // define( 'WP_DEBUG', false );
+        /*
+        //Contact Form 7 Plugin Configuration
+        define ( 'WPCF7_LOAD_JS',  false ); // Added to disable JS loading
+        define ( 'WPCF7_LOAD_CSS', false ); // Added to disable CSS loading
+        define ( 'WPCF7_AUTOP',    false ); // Added to disable adding <p> & <br> in form output
+        */
+
+
+        // Register Autoloaders Loader
+        $theme_dir = get_template_directory();
+        include "$theme_dir/library/library-loader.php";
+        include "$theme_dir/includes/includes-loader.php";
+        include "$theme_dir/components/components-loader.php";
+    }
 }
 
 
@@ -134,11 +134,11 @@ if ( ! function_exists( 'theme_setup' ) ) {
  * @since 1.0
  */
 if ( ! function_exists( 'theme_styles' ) ) {
-	function theme_styles() {
-		$theme_dir = get_stylesheet_directory_uri();
+    function theme_styles() {
+        $theme_dir = get_stylesheet_directory_uri();
 
-		wp_enqueue_style( 'main', "$theme_dir/assets/css/main.css", array(), null, 'all' );
-	}
+        wp_enqueue_style( 'main', "$theme_dir/assets/css/main.css", array(), null, 'all' );
+    }
 }
 
 
@@ -149,11 +149,12 @@ if ( ! function_exists( 'theme_styles' ) ) {
  * @since 1.0
  */
 if ( ! function_exists( 'theme_scripts' ) ) {
-	function theme_scripts() {
-		$theme_dir = get_stylesheet_directory_uri();
+    function theme_scripts() {
+        $theme_dir = get_stylesheet_directory_uri();
 
-		wp_enqueue_script( 'main', "$theme_dir/assets/js/main.js", array(), null, true );
-	}
+        wp_enqueue_script( '$', "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js", array(), null, true );
+        wp_enqueue_script( 'main', "$theme_dir/assets/js/main.js", array('$'), null, true );
+    }
 }
 
 
@@ -164,26 +165,26 @@ if ( ! function_exists( 'theme_scripts' ) ) {
  * @since 3.12.0
  */
 if ( ! function_exists( 'theme_scripts_localize' ) ) {
-	function theme_scripts_localize() {
-		$ajax_url_params = array();
+    function theme_scripts_localize() {
+        $ajax_url_params = array();
 
-		// You can remove this block if you don't use WPML
-		if ( function_exists( 'wpml_object_id' ) ) {
-			/** @var $sitepress SitePress */
-			global $sitepress;
+        // You can remove this block if you don't use WPML
+        if ( function_exists( 'wpml_object_id' ) ) {
+            /** @var $sitepress SitePress */
+            global $sitepress;
 
-			$current_lang = $sitepress->get_current_language();
-			wp_localize_script( 'main', 'i18n', array(
-				'lang' => $current_lang
-			) );
+            $current_lang = $sitepress->get_current_language();
+            wp_localize_script( 'main', 'i18n', array(
+                'lang' => $current_lang
+            ) );
 
-			$ajax_url_params['lang'] = $current_lang;
-		}
+            $ajax_url_params['lang'] = $current_lang;
+        }
 
-		wp_localize_script( 'main', 'urls', array(
-			'home'  => home_url(),
-			'theme' => get_stylesheet_directory_uri(),
-			'ajax'  => add_query_arg( $ajax_url_params, admin_url( 'admin-ajax.php' ) )
-		) );
-	}
+        wp_localize_script( 'main', 'urls', array(
+            'home'  => home_url(),
+            'theme' => get_stylesheet_directory_uri(),
+            'ajax'  => add_query_arg( $ajax_url_params, admin_url( 'admin-ajax.php' ) )
+        ) );
+    }
 }
