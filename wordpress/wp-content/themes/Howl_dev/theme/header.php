0cb0f49d82ab8c202c855467a0d516d15b563940
Brady Wlliams <bradymwilliams@gmail.com>
2016-11-01 16:53:46 -0600

    adding topbar -- 50% done

--- wordpress/wp-content/themes/Howl_dev/theme/header.php
+++ wordpress/wp-content/themes/Howl_dev/theme/header.php
@@ -8,27 +8,36 @@
 <!doctype html>
 <html class="site no-js" <?php language_attributes(); ?>>
 <head>
-	<!--[if lt IE 9]>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<![endif]-->
+    <!--[if lt IE 9]>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <![endif]-->
 
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	
-	<title><?php wp_title(); ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    
+    <title><?php wp_title(); ?></title>
 
-	<?php // replace the no-js class with js on the html element ?>
-	<script>document.documentElement.className=document.documentElement.className.replace(/\bno-js\b/,'js')</script>
+    <?php // replace the no-js class with js on the html element ?>
+    <script>document.documentElement.className=document.documentElement.className.replace(/\bno-js\b/,'js')</script>
 
-	<?php // load the core js polyfills ?>
-	<script async defer src="<?php echo get_template_directory_uri(); ?>/assets/js/core.js"></script>
+    <?php // load the core js polyfills ?>
+    <script async defer src="<?php echo get_template_directory_uri(); ?>/assets/js/core.js"></script>
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 <body class="site__body">
 <?php // <body> closes in footer.php ?>
-
+    
 
 
 <?php // common header content goes here ?>
+    <header class="primary" role="banner">
+        <h1>Test</h1>
+    </header>
+    <div class="cd-nav">
+        <a href="#0" class="cd-nav-trigger">Menu<span></span></a>
+        <?php MOZ_Menu::nav_menu( 'primary' ); ?>
+    </div>
+
+    <div style="height: 1600px"></div>
